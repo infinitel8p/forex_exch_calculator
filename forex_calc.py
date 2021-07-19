f57bfb92d04a8247fc8c1372d8b4eed170601011
Ludo <50703696+infinitel8p@users.noreply.github.com>
2021-07-19 19:19:41 +0200

    update forex_calc.py

--- forex_calc.py
+++ forex_calc.py
@@ -4,12 +4,8 @@ import time
 
 url_hrktoeur = "https://free.currconv.com/api/v7/convert?q=HRK_EUR&compact=ultra&apiKey=75f83697d28865fde0f4"
 url_eurtohrk = "https://free.currconv.com/api/v7/convert?q=EUR_HRK&compact=ultra&apiKey=75f83697d28865fde0f4"
-hrktoeur = 0.13
-eurtohrk = 7.51
 
-response = requests.get(url_hrktoeur)
-print()
-
+user_input = float(input("Insert a value: "))
 
 try:
     response = requests.get(url_hrktoeur)
@@ -17,15 +13,16 @@ try:
     if response.status_code == requests.codes.ok:
         hrktoeur = response.json()["HRK_EUR"]
         eurtohrk = requests.get(url_eurtohrk).json()["EUR_HRK"]
+        print(f"1 EUR = {eurtohrk} HRK")
+        print(f"1 HRK = {hrktoeur} EUR")
+        exchange = user_input * hrktoeur, user_input * eurtohrk
     else:
+      hrktoeur = 0.13
+      eurtohrk = 7.51
       print ("DISCONNECTED")
+      exchange = user_input * hrktoeur, user_input * eurtohrk
 except Exception as e:
     print ("NO INTERNET")
     print (str(e))
-
-
-user_input = float(input("Insert a value: "))
-
-exchange = user_input * hrktoeur, user_input * eurtohrk
 
 print(exchange)
