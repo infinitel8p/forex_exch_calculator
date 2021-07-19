1f764a7b1d8ea6198593f5fa26bb3f8cc54dbe89
Ludo <50703696+infinitel8p@users.noreply.github.com>
2021-07-19 20:52:26 +0200

    update forex_calc.py

--- forex_calc.py
+++ forex_calc.py
@@ -5,7 +5,8 @@ import time
 url_hrktoeur = "https://free.currconv.com/api/v7/convert?q=HRK_EUR&compact=ultra&apiKey=75f83697d28865fde0f4"
 url_eurtohrk = "https://free.currconv.com/api/v7/convert?q=EUR_HRK&compact=ultra&apiKey=75f83697d28865fde0f4"
 
-user_input = float(input("Insert a value: "))
+user_input = float(input("Insert a value: ")
+print("")
 
 try:
     response = requests.get(url_hrktoeur)
@@ -14,17 +15,17 @@ try:
         hrktoeur = response.json()["HRK_EUR"]
         eurtohrk = requests.get(url_eurtohrk).json()["EUR_HRK"]
         print(f"1 EUR = {eurtohrk} HRK")
-        print(f"1 HRK = {hrktoeur} EUR")
+        print(f"1 HRK = {hrktoeur} EUR\n")
         exchange = user_input * hrktoeur, user_input * eurtohrk
     else:
       hrktoeur = 0.13
       eurtohrk = 7.51
       print ("DISCONNECTED")
-      exchange = user_input * hrktoeur, user_input * eurtohrk
+      exchange = f"EUR: {user_input * hrktoeur}\nHKR: {user_input * eurtohrk}"
 except Exception as e:
     hrktoeur = 0.13
     eurtohrk = 7.51
     print ("NO INTERNET")
-    exchange = user_input * hrktoeur, user_input * eurtohrk
+    exchange = f"EUR: {user_input * hrktoeur}\nHKR: {user_input * eurtohrk}"
 
 print(exchange)
