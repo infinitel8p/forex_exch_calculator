c6ed89e17fa53a4fa1367233d4c9ab1ba7677c5f
Ludo <50703696+infinitel8p@users.noreply.github.com>
2021-07-19 18:44:20 +0200

    update forex_calc.py

--- forex_calc.py
+++ forex_calc.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 url_hrktoeur = "https://free.currconv.com/api/v7/convert?q=HRK_EUR&compact=ultra&apiKey=75f83697d28865fde0f4"
 url_eurtohrk = "https://free.currconv.com/api/v7/convert?q=EUR_HRK&compact=ultra&apiKey=75f83697d28865fde0f4"
@@ -7,30 +8,24 @@ hrktoeur = 0.13
 eurtohrk = 7.51
 
 response = requests.get(url_hrktoeur)
-print(response.json()["HRK_EUR"])
-response = requests.get(url_eurtohrk)
-print(response.json()["EUR_HRK"])
+print()
 
 
+try:
+    response = requests.get(url_hrktoeur)
+    print(f"Status code: {int(response.status_code)})
+    if response.status_code == requests.codes.ok:
+        hrktoeur = response.json()["HRK_EUR"]
+        eurtohrk = requests.get(url_eurtohrk).json()["EUR_HRK"]
+    else:
+      print ("DISCONNECTED")
+except Exception as e:
+    print ("NO INTERNET")
+    print (str(e))
 
-user_input = float(input("Wie viele Kuna? "))
 
-exchange = user_input * hrktoeur
+user_input = float(input("Insert a value: "))
+
+exchange = user_input * hrktoeur, user_input * eurtohrk
 
 print(exchange)
-
-import requests
-import time
-
-while True:
-    try:
-        response = requests.get(url_hrktoeur)
-        print(int(response.status_code))
-        if response.status_code == requests.codes.ok:
-            hrktoeur = 0.13
-        else:
-                print ("DISCONNECTED")
-        time.sleep(1)
-    except Exception as e:
-        print ("NO INTERNET")
-        print (str(e))
