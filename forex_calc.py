0a00e52aa4432a8858139210627f96179899af74
Ludo <50703696+infinitel8p@users.noreply.github.com>
2021-07-19 20:56:17 +0200

    update forex_calc.py

--- forex_calc.py
+++ forex_calc.py
@@ -5,18 +5,17 @@ import time
 url_hrktoeur = "https://free.currconv.com/api/v7/convert?q=HRK_EUR&compact=ultra&apiKey=75f83697d28865fde0f4"
 url_eurtohrk = "https://free.currconv.com/api/v7/convert?q=EUR_HRK&compact=ultra&apiKey=75f83697d28865fde0f4"
 
-user_input = float(input("Insert a value: ")
-print("")
+user_input = float(input("Insert a value: "))
 
 try:
     response = requests.get(url_hrktoeur)
-    print(f"Status code: {int(response.status_code)}")
+    print(f"\nStatus code: {int(response.status_code)}")
     if response.status_code == requests.codes.ok:
         hrktoeur = response.json()["HRK_EUR"]
         eurtohrk = requests.get(url_eurtohrk).json()["EUR_HRK"]
         print(f"1 EUR = {eurtohrk} HRK")
         print(f"1 HRK = {hrktoeur} EUR\n")
-        exchange = user_input * hrktoeur, user_input * eurtohrk
+        exchange = f"EUR: {user_input * hrktoeur}\nHKR: {user_input * eurtohrk}"
     else:
       hrktoeur = 0.13
       eurtohrk = 7.51
